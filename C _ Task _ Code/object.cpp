c151e2563885d1eec7c69d8f46a84529af632b08
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-19 12:31:31 +0530

    Create object.cpp

---
+++ C _ Task _ Code/object.cpp
@@ -0,0 +1,32 @@
+// Structure 
+
+#include<stdio.h>
+
+struct studinfo
+{
+	int id;
+	char name[10];
+		
+}
+ details,details2; // object of structure 
+int main()
+{	
+	printf("ID-->\n");
+	scanf("%d",&details.id);
+	printf("Name-->\n");
+	scanf("%s",&details.name);
+	
+	printf("Name-->%s",details.name);
+	printf("ID-->%d",details.id);
+	
+	printf("-__-");
+	printf("ID-->\n");
+	scanf("%d",&details2.id);
+	printf("Name-->\n");
+	scanf("%s",&details2.name);
+	
+	printf("Name-->%s",details2.name);
+	printf("ID-->%d",details2.id);
+
+
+} 
