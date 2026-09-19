5e054939ebcb0245ad0670c944d7db71a8fe16d0
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-19 12:57:28 +0530

    Create S10_T01.cpp

---
+++ C _ Assignment _ Code/Session - 10/S10_T01.cpp
@@ -0,0 +1,17 @@
+/*
+	Declare a string variable called 
+	songTitle and assign it the value 'Tum Hi Ho'. 
+	Print the length of the string using strlen().
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char songTitle[] = "Tum Hi Ho";	
+	printf("Song Title = %s",songTitle);
+	printf("\n");
+	printf("\nLength of Song Title = %d",strlen(songTitle));
+	return 0;
+}
