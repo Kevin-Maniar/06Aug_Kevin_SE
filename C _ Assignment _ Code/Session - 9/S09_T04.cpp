7435c0eb142c3025eed70cc76a4fb5c89a64eb68
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-19 18:03:56 +0530

    Update S09_T04.cpp

--- C _ Assignment _ Code/Session - 9/S09_T04.cpp
+++ C _ Assignment _ Code/Session - 9/S09_T04.cpp
@@ -9,6 +9,30 @@
 #include<stdio.h>
 int main()
 {
-	int cricketScores[][];
+	int cricketScores[3][3];
+	int i,j;
+	
+	for (i=0;i<3;i++)
+	{
+		printf("Number of Match:%d\n",i+1);
+		for(j=0;j<3;j++)
+	{
+		printf("Enter Score of Team%d:\n",j+1);
+		scanf("%d",&cricketScores[i][j]);
+	}
+	}
+	
+	printf("\tCricket Scores\n");
+	
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",cricketScores[i][j]);
+		}
+		printf("\n");	
+	}
+	
+	
 	return 0;
 }
