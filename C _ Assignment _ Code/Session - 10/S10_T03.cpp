832fd5a9ea74f8e2c6beb7773e0720d070c8d210
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-19 12:59:55 +0530

    Create S10_T03.cpp

---
+++ C _ Assignment _ Code/Session - 10/S10_T03.cpp
@@ -0,0 +1,8 @@
+/*
+	Copy the string 'Flipkart' into another string 
+	Make sure to declare enough space for the destination string.
+	variable called shoppingApp using strcpy(), 
+	then print the value of shoppingApp.
+	Hint:
+	Make sure to declare enough space for the destination string.
+*/
