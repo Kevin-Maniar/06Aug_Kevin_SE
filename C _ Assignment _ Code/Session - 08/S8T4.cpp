edb6122e7b30902446f41c5b1fe37f74beef6aea
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-22 17:35:41 +0530

    initial commit

--- C _ Assignment _ Code/Session - 08/S8T4.cpp
+++ C _ Assignment _ Code/Session - 08/S8T4.cpp
@@ -4,3 +4,24 @@
 	(e.g., '?1,599'). 
 	Use this function to display prices for three different products.
 */
+
+#include<stdio.h>
+
+int formatprice(int price,char result[30])
+{
+	printf("Price:%d\n",price);
+	sprintf(result,"$%d",price);
+}
+int main()
+{
+	int price;
+	char result[20];
+	
+	printf("Enter Price:");
+	scanf("%d",&price);
+	
+	formatprice(price,result);
+	
+	printf("Price: %s",result);
+	return 0;
+}
--- C _ Assignment _ Code/Session - 08/S8T5.cpp
+++ C _ Assignment _ Code/Session - 08/S8T5.cpp
@@ -5,3 +5,28 @@
 	Constraint:
 	The refactored function should work for any string input, not just a specific use case.
 */
+#include <stdio.h>
+#include <ctype.h>
+
+void capitalize(char str[])
+{
+    str[0] = toupper(str[0]);
+}
+
+int main()
+{
+    char names[4][50] = {
+        "apple",
+        "rahul",
+        "banana",
+        "john"
+    };
+
+    for (int i = 0; i < 4; i++)
+    {
+        capitalize(names[i]);
+        printf("%s\n", names[i]);
+    }
+    return 0;
+}
+
