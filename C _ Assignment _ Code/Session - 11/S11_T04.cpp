a1255991719379ddbc82b2b6b7949fcd6b1e3c0b
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-23 11:44:12 +0530

    Update S11_T04.cpp

--- C _ Assignment _ Code/Session - 11/S11_T04.cpp
+++ C _ Assignment _ Code/Session - 11/S11_T04.cpp
@@ -8,9 +8,21 @@
 #include<stdio.h>
 int increFollower(int *follower,int n)
 {
-	
+	for(int i=0;i<n;i++)
+	{
+		*(follower+i) += 100;	// follower[i] += 100 alt;;;;
+	}	
 }
 int main()
 {
+	int follower[5] = {100,200,300,400,500};
 	
+	increFollower(follower,5);
+	
+	printf("Updated Followers:)<^>\n");
+	
+	for(int i=0;i<5;i++)
+	{
+		printf("Followers: %d\n",follower[i]);
+	}
 }
