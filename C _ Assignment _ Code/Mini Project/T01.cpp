86bb820a2927d297b16d9a305ba1285a71749e84
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-23 21:49:47 +0530

    initial commit

--- C _ Assignment _ Code/Mini Project/T01.cpp
+++ C _ Assignment _ Code/Mini Project/T01.cpp
@@ -4,3 +4,24 @@
 	they listened to music each day 
 	for a week and stores this data in an array.
 */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("\n>->->Welcome to Music Listening App<-<-<\n\n");
+	
+	int min[100];
+	int i;
+	printf("Enter the number of minutes you listen to music each days\n\n");
+	for(i=1;i<=7;i++)
+	{
+		printf("Day %d:-",i);
+		scanf("%d",&min[i]);
+	}
+	
+	for(i=1;i<=7;i++)
+	{
+		printf("Your day %d music play time:- %d Minutes\n",i,min[i]);
+	}
+}
--- C _ Assignment _ Code/Mini Project/T03.cpp
+++ C _ Assignment _ Code/Mini Project/T03.cpp
@@ -4,3 +4,71 @@
 	the data is saved to a file 
 	named music_log.txt for persistence.
 */
+
+#include<stdio.h>
+
+int main()
+{	
+	int min[100];
+	int i,choice;
+
+	printf("----------------------------------------------\n");
+	printf("Welcome to Music Listening App\n");
+	printf("----------------------------------------------\n");
+	while(1)
+	{
+		printf("Press 1 to Add your Loggs\n");
+		printf("Press 2 to view Your Loggs\n");
+		printf("Press 3 to Log out\n");
+		printf("----------------------------------------------");
+		printf("\n");
+		scanf("%d",&choice);
+		printf("----------------------------------------------\n");
+	
+	
+		switch(choice)
+		{
+			case 1:
+			
+				for(i=1;i<=7;i++)
+			{
+				
+				printf("Day %d:-",i);
+				scanf("%d",&min[i]);	
+			}
+				printf("----------------------------------------------\n");
+			break ;
+			
+			case 2:
+				printf("----------------------------------------------\n");
+				printf("Your Added Log\n");
+				printf("----------------------------------------------\n");
+			for(i=1;i<=7;i++)
+			{
+				printf("Your day %d music play time:- %d Minutes\n",i,min[i]);
+				printf("\n");
+			}
+				printf("----------------------------------------------\n");
+			break;
+			
+			case 3:
+				printf("----------------------------------------------");
+				printf("\nLogging out! Visit Again\n");
+				printf("----------------------------------------------");
+				printf("\n");
+			break;
+		
+			default:
+				printf("----------------------------------------------");
+				printf("\nError!!!! Please go with 1  or 2 or 3 only\n");
+				printf("----------------------------------------------");
+				printf("\n");
+			break;
+		}
+		
+			if(choice==3)
+			{
+		 	break;
+	    	}		
+  	}
+}
