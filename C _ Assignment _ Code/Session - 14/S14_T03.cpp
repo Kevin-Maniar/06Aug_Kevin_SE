36a9758a952109fb78b39d3becdf0dc1123680f6
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-23 11:50:13 +0530

    Update S14_T03.cpp

--- C _ Assignment _ Code/Session - 14/S14_T03.cpp
+++ C _ Assignment _ Code/Session - 14/S14_T03.cpp
@@ -7,4 +7,24 @@
 	1000 as-is. 
 	Add clear comments and use proper indentation.
 */
-
+#include<stdio.h>
+int main()
+{
+	float i;
+	printf("Dear User Enter Your Followers:");
+	scanf("%f",&i);
+	
+	
+	if(i>1000000)
+	{
+		printf("Your Followers: %.1fM",i/1000000.0);
+	}
+	else if(i>=1000)
+	{
+		printf("Your Followers: %.1fK",i/1000.0);
+	}
+	else
+	{
+		printf("Your Followers: %f",i);
+	}
+}
