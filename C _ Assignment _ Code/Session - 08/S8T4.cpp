5fc654d64f13b349f08ae981582a99991134d8ca
Kevin-Maniar <kevinmaniar.mkj@gmail.com>
2026-09-23 11:48:09 +0530

    Update S8T4.cpp

--- C _ Assignment _ Code/Session - 08/S8T4.cpp
+++ C _ Assignment _ Code/Session - 08/S8T4.cpp
@@ -1,9 +1,12 @@
 /*
 	Build a function formatPrice that takes a price 
 	in rupees and returns a string formatted like Flipkart's price tag 
-	(e.g., '?1,599'). 
+	(e.g., '$1,599'). 
 	Use this function to display prices for three different products.
 */
+
+
+// major logic pending 
 
 #include<stdio.h>
 
